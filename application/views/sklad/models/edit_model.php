14e439d9a30ecc8927372e3a0da3ca71ec7f8cba
Evgeny Lizhenin <elizhenin@gmail.com>
2015-03-20 16:57:01 +0300

    загрузка и управление изображениями к моделям

--- application/views/sklad/models/edit_model.php
+++ application/views/sklad/models/edit_model.php
@@ -253,7 +253,7 @@
                                    <label><input type="checkbox" name="delete_<?=$image['id']?>"/>Удалить</label>
                                </td>
                                <td><img alt="" src="/images/sklad/<?= $image['file'] ?>"
-                                        style="width:100px;height:100px;"></td>
+                                        style="max-height:200px;"></td>
                                <td><input type="text" value="<?=$image['alt']?>" name="alt_<?=$image['id']?>"/></td>
 
                                <td><input type="checkbox" <?= (empty($image['important']))?'':'checked="checked"' ?> name="important_<?=$image['id']?>"/></td>

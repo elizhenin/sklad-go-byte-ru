7ab8f986126e0b94574c512f9f80e1417bdad4af
Sergey Musatov <akapulse@yandex.ru>
2015-04-01 16:59:03 +0300

    config DB

--- application/config/database.php
+++ application/config/database.php
@@ -19,9 +19,9 @@ return array
 			 * Ports and sockets may be appended to the hostname.
 			 */
 			'hostname'   => 'localhost',
-			'database'   => 'sklad',
-			'username'   => 'sklad',
-			'password'   => 'Pass123',
+			'database'   => 'gobyte_studio',
+			'username'   => 'gobyte_studio',
+			'password'   => 'Y4UI54ew',
 			'persistent' => FALSE,
 		),
 		'table_prefix' => 'skl_',

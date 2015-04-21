e5159be77e91fb7fe8cde28c6c147449ca9ec599
Evgeny Lizhenin <elizhenin@gmail.com>
2015-04-21 16:29:26 +0300

    контентщику доступен только раздел модели и товары

--- application/views/sklad/menu/content.php
+++ application/views/sklad/menu/content.php
@@ -10,7 +10,4 @@
     <li class="icon-products <?=(Request::current()->action()=='products')?'menu_active':''?>">
         <a href="/sklad/products"><span></span><br>Товары</a>
     </li>
-    <li class="icon-orders <?=(Request::current()->action()=='orders')?'menu_active':''?>">
-        <a href="/sklad/orders"><span></span><br>Продажи</a>
-    </li>
 </ul>
